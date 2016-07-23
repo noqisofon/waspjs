3f9dbc4d4d820a7da689115399a69b76ddf8427a
ned rihine <ned.rihine@gmail.com>
2016-07-23 23:48:23 +0900

    lint base on fix

--- test/emitter.js
+++ test/emitter.js
@@ -1,26 +1,26 @@
 // const util = require( 'util' );
-const tape = require( 'tape' );
+const tape = require('tape');
 
-const Emitter = require( '../lib/emitter' );
+const Emitter = require('../lib/emitter');
 
 class Subscriber extends Emitter {
 }
 
-const my_event = 'my_event';
+const myEvent = 'my_event';
 
-tape.test( '->  Emitter', t => {
-    t.ok( Emitter !== undefined, 'is defined' );
+tape.test('->  Emitter', t => {
+    t.ok(Emitter !== undefined, 'is defined');
 
     {
         const aSubscriber = new Subscriber();
-        
-        t.deepEqual( aSubscriber.events, [], 'inherit events collection' );
 
-        aSubscriber.on( my_event, data => {
-            t.deepEqual( data, { data: 1 }, 'notify events' );
+        t.deepEqual(aSubscriber.events, [], 'inherit events collection');
+
+        aSubscriber.on(myEvent, data => {
+            t.deepEqual(data, {data: 1}, 'notify events');
             t.end();
-        } );
+        });
 
-        aSubscriber.emit( my_event, { data: 1 } );
+        aSubscriber.emit(myEvent, {data: 1});
     }
-} );
+});
--- test/wasp.js
+++ test/wasp.js
@@ -1,43 +1,44 @@
-const fs   = require( 'fs' );
-const path = require( 'path' );
+// const fs       = require( 'fs' );
+const path = require('path');
+const process = require('process');
 
-const tape = require( 'tape' );
+const tape = require('tape');
 
-const Wasp = require( '../lib/wasp' );
+const Wasp = require('../lib/wasp');
 
-const fixtures = path.join( process.cwd(), 'test', 'fixtures' );
-const waspfile  = path.join( fixtures, 'waspfile.js' );
+const fixtures = path.join(process.cwd(), 'test', 'fixtures');
+const waspfile = path.join(fixtures, 'waspfile.js');
 
-tape.test( '->  Wasp', t => {
-    t.ok( Wasp !== undefined, 'is defined' );
+tape.test('->  Wasp', t => {
+    t.ok(Wasp !== undefined, 'is defined');
 
-    let methods = [
+    const methods = [
         'from', 'filter', 'watch', 'unwrap', 'exec',
-        'start', 'write', 'clean', 'concat', 'to'  , 'emit'
+        'start', 'write', 'clean', 'concat', 'to', 'emit'
     ];
 
-    methods.forEach( method => {
-        let actual_method = Wasp.prototype[method];
+    methods.forEach(method => {
+        const actualMethod = Wasp.prototype[method];
 
-        t.ok( actual_method !== undefined, method + ' is defined' );
-    } );
+        t.ok(actualMethod !== undefined, method + ' is defined');
+    });
 
     t.end();
-} );
+});
 
-tape.test( '->  Wasp.constructor', t => {
-    let wasp = new Wasp( {
+tape.test('->  Wasp.constructor', t => {
+    const wasp = new Wasp({
         file: waspfile,
         host: {
             task: () => {
-                t.equal( wasp, this, 'bind tasks to wasp instance' );
+                t.equal(wasp, this, 'bind tasks to wasp instance');
             }
         }
-    } );
+    });
 
-    t.ok( wasp !== undefined, 'create wasp instance' );
+    t.ok(wasp !== undefined, 'create wasp instance');
 
-    t.ok( wasp.tasks.task !== undefined, 'load task from host' );
+    t.ok(wasp.tasks.task !== undefined, 'load task from host');
 
     t.end();
-} );
+});
